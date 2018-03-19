e4a6b71c6189d32e58a27cb2df690b4b798823b2
pongsanti <pongsanti.tanvejsilp@gmail.com>
2018-03-19 15:46:30 +0700

    SET DISPLAY=:0 at the top of crontab file / fix waiting vlc window to be ready

--- kill.sh
+++ kill.sh
@@ -2,3 +2,6 @@
 vlc/kill_vlc.sh
 kill -9 $(cat first.pid)
 kill -9 $(cat second.pid)
+
+rm vlc/vlc.pid
+rm windowid/vlcwindow.id
--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 vlc/run_vlc.sh
-sleep 8 # wait window to open
 windowid/findwinid.sh
 ruby first.rb &
 echo $! > first.pid
